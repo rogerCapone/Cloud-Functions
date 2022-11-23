add57bc15dda1a1e4cb19a3bd364242382bf6039
Roger Capone <60660530+rogerCapone@users.noreply.github.com>
2022-11-23 18:27:35 +0100

    Create searchUser.ts

---
+++ UFly/searchUser.ts
@@ -0,0 +1,99 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+//const https = require('https');
+const  now = require("performance-now")
+const request = require("request");
+// const {PubSub} = require('@google-cloud/pubsub');
+const {Storage} = require('@google-cloud/storage');
+const storage = new Storage();
+// const puppeteer = require('puppeteer');
+// const pubSubClient = new PubSub();
+
+//const d = new Date();
+//const curr_month = d.getMonth() + 1; //Months are zero based
+//const curr_year = d.getFullYear();
+//const date = (curr_year + '-' + curr_month + '-' + curr_date);
+
+//myImports
+//import {AviationStack} from './AviationStack';
+import {AviationStack2} from './AviationStack2';
+import {Airport} from './Airport';
+import {ArrivalTimeTable} from './ArrivalTimeTable';
+import {Airlines} from './Airlines';
+import {Score, ScoreData} from './ScoreData';
+import {TripAdvisorPollResp} from './TripAdvisorPollResp';
+// import {Ticket, PVenta} from './Tickets';
+import {User} from './UserList';
+import {KiwiResponse} from './kiwiResponse';
+import {KiwiTicket} from './kiwiTickets';
+//end myimport
+
+export class Convert {
+    public static toAviationStack2(json: string): AviationStack2 {
+        return JSON.parse(json);
+    }
+}
+export class Converted {
+    public static toAirlines(json: string): Airlines {
+        return JSON.parse(json);
+    }
+}
+export class Convertion {
+    public static toArrivalTimeTable(json: string): ArrivalTimeTable {
+        return JSON.parse(json);
+    }
+}
+export class Converting {
+    public static toAirport(json: string): Airport {
+        return JSON.parse(json);
+    }
+}
+export class Conversion {
+ public static toScore(json: string): Score[] {
+     return JSON.parse(json);
+ }
+ public static scoreToJson(value: Score[]): string {
+       return JSON.stringify(value);
+   }
+}
+export class ConvertTransf {
+    public static toTripAdvisorPollResp(json: string): TripAdvisorPollResp {
+        return JSON.parse(json);
+    }}
+export class Convertir {
+    public static toKiwiResponse(json: string): KiwiResponse {
+        return JSON.parse(json);
+    }}
+
+admin.initializeApp();
+const db = admin.firestore();
+const fcm = admin.messaging();
+const access_key = 'API_KEY'; 
+
+
+
+exports.searchUser = functions.https.onCall(async (data, context) => {
+
+  const userToFind = data.userToFind;
+  const uid = context.auth?.uid;
+
+  return db.doc('allUsers/usersDoc').get().then((allUsersDocu)=>{
+
+    const allUsers = allUsersDocu.data()?.userList;
+    let usersMatching: User[] = new Array();
+
+    for (let index = 0; index < allUsers.length; index++) {
+      if(allUsers[index].nickName.toString().toUpperCase().includes(userToFind.toUpperCase())){
+        if(allUsers[index].uid == uid){
+          console.log('Users profile, no need to display')
+        }else{
+        usersMatching.push({img:allUsers[index].img, nickName:allUsers[index].nickName ,uid: allUsers[index].uid});
+        console.log(`${allUsers[index].nickName} inserted to the ARRAY`);
+      }}
+      console.log(`Evaluating ${allUsers[index].nickName}`)
+    }
+    return usersMatching;
+  })
+  .catch(console.error);
+})
