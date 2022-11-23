7e0f0a56e6b9ad206ffcfa4b8971c70d3683abb0
Roger Capone <60660530+rogerCapone@users.noreply.github.com>
2022-11-23 18:32:14 +0100

    Create declineFriend.ts

---
+++ UFly/declineFriend.ts
@@ -0,0 +1,86 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+//const https = require('https');
+const  now = require("performance-now")
+const request = require("request");
+// const {PubSub} = require('@google-cloud/pubsub');
+const {Storage} = require('@google-cloud/storage');
+const storage = new Storage();
+// const puppeteer = require('puppeteer');
+// const pubSubClient = new PubSub();
+
+//const d = new Date();
+//const curr_month = d.getMonth() + 1; //Months are zero based
+//const curr_year = d.getFullYear();
+//const date = (curr_year + '-' + curr_month + '-' + curr_date);
+
+//myImports
+//import {AviationStack} from './AviationStack';
+import {AviationStack2} from './AviationStack2';
+import {Airport} from './Airport';
+import {ArrivalTimeTable} from './ArrivalTimeTable';
+import {Airlines} from './Airlines';
+import {Score, ScoreData} from './ScoreData';
+import {TripAdvisorPollResp} from './TripAdvisorPollResp';
+// import {Ticket, PVenta} from './Tickets';
+import {User} from './UserList';
+import {KiwiResponse} from './kiwiResponse';
+import {KiwiTicket} from './kiwiTickets';
+//end myimport
+
+export class Convert {
+    public static toAviationStack2(json: string): AviationStack2 {
+        return JSON.parse(json);
+    }
+}
+export class Converted {
+    public static toAirlines(json: string): Airlines {
+        return JSON.parse(json);
+    }
+}
+export class Convertion {
+    public static toArrivalTimeTable(json: string): ArrivalTimeTable {
+        return JSON.parse(json);
+    }
+}
+export class Converting {
+    public static toAirport(json: string): Airport {
+        return JSON.parse(json);
+    }
+}
+export class Conversion {
+ public static toScore(json: string): Score[] {
+     return JSON.parse(json);
+ }
+ public static scoreToJson(value: Score[]): string {
+       return JSON.stringify(value);
+   }
+}
+export class ConvertTransf {
+    public static toTripAdvisorPollResp(json: string): TripAdvisorPollResp {
+        return JSON.parse(json);
+    }}
+export class Convertir {
+    public static toKiwiResponse(json: string): KiwiResponse {
+        return JSON.parse(json);
+    }}
+
+admin.initializeApp();
+const db = admin.firestore();
+const fcm = admin.messaging();
+const access_key = 'API_KEY'; 
+
+
+export const declineFriend = functions.https.onCall(async (data, context) => {
+
+  const uid = data.uid;           //req.query.uid;
+  const noFriend =  data.noFriend;       // req.query.noFriend;
+
+  return db.doc(`users/${uid}`).update({
+    pendingToAccept: admin.firestore.FieldValue.arrayRemove(noFriend)
+  }).then(()=>{
+    console.log(`${noFriend} is like shit! I dont want it as friend...`);
+    return true;
+  }).catch(console.error);
+})
