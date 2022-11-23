36dbc8a15225ec103f5550005bff29309448758a
Roger Capone <60660530+rogerCapone@users.noreply.github.com>
2022-11-23 17:39:17 +0100

    Update index.ts

--- FlightBroker/index.ts
+++ FlightBroker/index.ts
@@ -1,4 +1,53 @@
+
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
-//?My imports
+const request = require("request");
+
+import {KiwiTicket} from './kiwiTickets';
+import {KiwiResponse} from './kiwiResponse';
+
+
+
+
+admin.initializeApp();
+const db = admin.firestore();
+const fcm = admin.messaging();
+
+
+
+export class Convertir {
+  public static toKiwiResponse(json: string): KiwiResponse {
+      return JSON.parse(json);
+  }}
+
+
+exports.onUserWelcome = functions.auth.user().onCreate(async (user) => {
+  
+
+    return db.doc(`Users/${user.uid}`).create({
+      uid: user.uid,
+      userEmail: user.email,
+      displayName: !user.displayName? user.displayName: user.email?.split('@')[0],
+      tokenId: "",
+      pendingToAccept:[],
+      photoUrl: user.photoURL,
+      myComms: [],
+      planSubscription: 0,
+      notificationsRegister: [],
+      subscriptionsRegister: []
+    }).then(()=>{
+  
+      return db.doc('allUsers/usersDoc').set({
+        totalUsers : admin.firestore.FieldValue.increment(+1),
+        userList : admin.firestore.FieldValue.arrayUnion({
+          uid: user.uid,
+          photoUrl: user.photoURL,
+          displayName: user.displayName
+        })
+      }, {merge:true});
+  
+    }).catch(err=>{
+        console.log(err)
+      })
+    })
