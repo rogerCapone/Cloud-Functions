8be461a50d3c1f954f18ebc1c13073c90c278275
Roger Capone <60660530+rogerCapone@users.noreply.github.com>
2022-11-23 17:59:07 +0100

    Create onGoodbye.ts

---
+++ UFly/onGoodbye.ts
@@ -0,0 +1,26 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+const request = require("request");
+
+import {KiwiTicket} from './kiwiTickets';
+import {KiwiResponse} from './kiwiResponse';
+
+
+
+
+admin.initializeApp();
+const db = admin.firestore();
+const fcm = admin.messaging();
+
+
+
+export class Convertir {
+  public static toKiwiResponse(json: string): KiwiResponse {
+      return JSON.parse(json);
+  }}
+
+
+exports.onUserGoodbye = functions.auth.user().onDelete((user, context)=>{
+      return db.doc(`Users/${user.uid}`).delete();
+  })
